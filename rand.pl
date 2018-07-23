b2abbf489dc5ae6fccadad73a387de3dc1ffe151
xxxboomerxxx <40468107+xxxboomerxxx@users.noreply.github.com>
2018-07-23 02:26:22 +0100

    fix broken shit
    :D

--- rand.pl
+++ rand.pl
@@ -5,14 +5,15 @@ use List::Util qw ( shuffle );
 
 my $current = "";
 my @items = qx % ls | grep -i -e '\\.png\$' -e '\\.jpg\$' -e '\\.gif\$' -e '\\.webm\$' %;
-
+chomp @items;
+@items = grep defined, @items;
 @items = shuffle @items;
-while(($current = shift @items) != undef) {
-    chomp($current);
-    my ($ext) = $current =~ /((\.[^.\s]+)+)$/;
+
+while(defined ($current = shift @items)) {
+    my ($ext) = $current =~ /\.(gif|webm|png|jpg|jpeg)$/;
     my $time = qx % date +\%s\%9N %;
     chomp($time);
-    my $new_file = $time . $ext;
-    qx % mv -f $current $new_file %;
+    my $new_file = $time . "." . $ext;
+    qx % mv -f "$current" "$new_file" %;
     @items = shuffle @items;
 }
